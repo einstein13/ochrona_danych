995c120d161e65e55262d5a868a13bde42cdd446
Jakub Rak <kartezjusz2009@tlen.pl>
2019-10-19 18:35:36 +0200

    encoding

---
+++ temat2/zestaw1/zadanie2.py
@@ -0,0 +1,67 @@
+from commons import *
+from json import dumps, loads
+from random import choice, shuffle
+
+chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+
+def generate_key(data):
+    result = []
+    used = []
+    for char in chars:
+        key = ""
+        while key == "" or key in used:
+            key = ""
+            key += choice(chars)
+            key += choice(chars)
+        result.append([char, key])
+    shuffle(result)
+    return result
+
+def read_or_generate_key(data):
+    try:
+        key = read_file("key2.txt")
+        key = loads(key)
+    except:
+        key = generate_key(data)
+        write_file(dumps(key), "key2.txt")
+    data['key'] = key
+    [data['chars'], data['keys']] = transpose(key)
+    return
+
+def initialize(data):
+    data['output'] = ''
+    data['input'] = read_file("input2.txt")
+    read_or_generate_key(data)
+    return
+
+def encode_message(data):
+    text = data['input']
+    encoded = ""
+    itr = 0
+    itr_max = len(text)
+    keys_max = len(data['chars'])
+    while itr < itr_max:
+        position = data['chars'].index(text[itr])
+        if position > -1:
+            position += itr
+            position = position % keys_max
+            encoded += data['keys'][position]
+        else:
+            encoded += text[itr]*2
+        itr += 1
+    data['output'] = encoded
+    return encoded
+
+def decode_message(data):
+    return
+
+def main():
+    data = {}
+    initialize(data)
+    command = find_value_input("Zakoduj: 1 / Rozkoduj: 2", "integer", allowed_values=[1, 2])
+    if command == 1:
+        encode_message(data)
+    elif command == 2:
+        decode_message(data)
+    return data['output']
