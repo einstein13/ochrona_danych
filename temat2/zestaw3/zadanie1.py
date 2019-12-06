e4b81c478acae60ed66261e37047e4918277173b
Jakub Rak <kartezjusz2009@tlen.pl>
2019-12-06 20:47:34 +0100

    zadania

---
+++ temat2/zestaw3/zadanie1.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+
+from commons import *
+
+def initialize(data):
+    data['output'] = ""
+    data['input'] = ""
+
+def main():
+    data = {}
+    initialize(data)
+
+    return data['output']
