cddf61b9b2be930edc061ff7764739da059b8c8a
Jakub Rak <kartezjusz2009@tlen.pl>
2019-12-16 11:25:11 +0100

    pesel check

--- temat2/zestaw4/zadanie1.py
+++ temat2/zestaw4/zadanie1.py
@@ -1,5 +1,4 @@
 # -*- coding: UTF-8 -*-
-
 from commons import *
 
 def initialize(data):
@@ -19,12 +18,73 @@ def validate_pesel(data):
 
     return True
 
+
 def pesel_sex(data):
     value = data['input_string'][9]
     value = int(value)
     if value % 2 == 0:
         return "kobieta"
     return "mężczyzna"
+
+def pesel_chksum(data):
+    value1 = data['input_string'][0]
+    value1 = int(value1)
+    value2 = data['input_string'][1]
+    value2 = int(value2)
+    value3 = data['input_string'][2]
+    value3 = int(value3)
+    value4 = data['input_string'][3]
+    value4 = int(value4)
+    value5 = data['input_string'][4]
+    value5 = int(value5)
+    value6 = data['input_string'][5]
+    value6 = int(value6)
+    value7 = data['input_string'][6]
+    value7 = int(value7)
+    value8 = data['input_string'][7]
+    value8 = int(value8)
+    value9 = data['input_string'][8]
+    value9 = int(value9)
+
+    value10 = data['input_string'][9]
+    value10 = int(value10)
+
+    value11 = data['input_string'][10]
+    value11 = int(value11)
+
+
+    suma = (9*(value1)) + (7*(value2)) + (3*(value3)) + (1*(value4)) + (9*(value5)) + (7*(value6)) + (3*(value7)) + (1*(value8)) + (9*(value9)) + (7*(value10))
+    suma = int(suma)
+    # print (suma)
+    # print (value11)
+
+    mod = suma % 10
+    # print (mod)
+
+    if mod == value11:
+         return "OK"
+    else:
+         return "FAIL"
+
+    #  kontrolka=10-(suma %10)
+    # print (kontrolka)
+
+
+ 
+    #kontrolka i sprawdzenie zgodnosci
+    # if ((kontrolka== 10)or(kontrolka== 0)):
+    #     return 0
+    # else:
+    #     return 1
+ 
+    
+
+    # if (kontrolka==10) or (data==kontrolka): #w przypadku liczby kontrolnej 10 i 0 sa jednoznaczne a 0 moze byc wynikiem odejmowania
+    #     return "DUPA" ##domyslana wartosc logczna dla ifa klasy roboczej
+    # else:
+    #     return "SUMA" ##domyslna wartosc logiczna dla elsa
+
+
 
 def main():
     data = {}
@@ -43,6 +103,7 @@ def main():
         if validated:
             print("Podany numer PESEL jest poprawny")
             print("Osoba z podanym numerem PESEL to %s" % pesel_sex(data))
+            print("Suma kontrolna %s" % pesel_chksum(data))
         
         else:
             print("Numer PESEL jest niepoprawny")
